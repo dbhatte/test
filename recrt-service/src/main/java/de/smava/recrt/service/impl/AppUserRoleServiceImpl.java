cb5b0623c82562dec304b118eeab03ca762d4efc
Devendra Bhatte <devendra.bhatte@home24.de>
2016-07-01 19:43:46 +0200

    introduce caching for the de.smava.recrt.service.AppUserRoleService.getByAppUser

--- recrt-service/src/main/java/de/smava/recrt/service/impl/AppUserRoleServiceImpl.java
+++ recrt-service/src/main/java/de/smava/recrt/service/impl/AppUserRoleServiceImpl.java
@@ -1,16 +1,18 @@
 package de.smava.recrt.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import de.smava.recrt.exception.RecrtServiceException;
 import de.smava.recrt.model.AppUser;
 import de.smava.recrt.model.AppUserRole;
 import de.smava.recrt.persistence.model.AppUserEntity;
 import de.smava.recrt.persistence.repository.AppUserRoleRepository;
 import de.smava.recrt.service.AppUserRoleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class AppUserRoleServiceImpl implements AppUserRoleService {
@@ -20,6 +22,7 @@ public class AppUserRoleServiceImpl implements AppUserRoleService {
 
     @Override
     @Transactional
+    @Cacheable("appUserRoles")
     public List<? extends AppUserRole> getByAppUser(AppUser appUser) throws RecrtServiceException {
         return repository.findByKeyAppUser((AppUserEntity) appUser);
     }
